2a08f1e24036a61e86f31a2d4af318b4138e6a6f
zarakikenpashi <sadiarog.abel@gmail.com>
2025-03-26 10:45:16 +0000

    correct logo link

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom'
 import logo from '../../assets/logo.svg'
 import Navigation from './Navigation'
 
 const Sidebar = () => {
   return (
     <aside className="h-full bg-neutral-100  w-full">
-        <a href="#" className='flex justify-center items-center p-1'>
+        <Link to="/dashboard" className='flex justify-center items-center p-1'>
             <img src={logo} alt="LOGO"  className='w-40' />
-        </a>
+        </Link>
          <Navigation />
     </aside>
   )
